2d147282cf0964b04ef959e0a43e023e9dc2d266
suvorovjr <pglavan1998@mail.ru>
2024-06-17 00:34:27 +0300

    add changes in settings.py

--- yelp/settings.py
+++ yelp/settings.py
@@ -57,9 +57,9 @@ ROBOTSTXT_OBEY = False
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-# DOWNLOADER_MIDDLEWARES = {
-#    "yelp.middlewares.YelpDownloaderMiddleware": 543,
-# }
+DOWNLOADER_MIDDLEWARES = {
+   "yelp.middlewares.RandomProxyMiddleware": 543,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
