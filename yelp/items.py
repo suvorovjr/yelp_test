26959881679be3512c8ce59c08d2a87fd5a7b188
suvorovjr <pglavan1998@mail.ru>
2024-06-17 01:58:41 +0300

    add docstrings in items.py

--- yelp/items.py
+++ yelp/items.py
@@ -7,6 +7,16 @@ import scrapy
 
 
 class ReviewItem(scrapy.Item):
+    """
+    Класс оперделения структуры данных для отзыва
+    :param username: Имя пользователя, оставишего отзыв
+    :param date: Дата отзыва
+    :param stars: Оценка отзыва
+    :param comment_text: Текст отзыва
+    :param image_urls: Ссылки на изображения, прикрепленные к отзыву
+    :param answer: Ответ на отзыв
+    """
+
     username = scrapy.Field()
     date = scrapy.Field()
     stars = scrapy.Field()
@@ -16,6 +26,13 @@ class ReviewItem(scrapy.Item):
 
 
 class AnswerItem(scrapy.Item):
+    """
+    Класс определения структуры ответа на отзыв
+    :param username: Имя пользователя, оставившего ответ на отзыв
+    :param answer_date: Дата ответа
+    :param answer_text: Текст ответа
+    """
+
     username = scrapy.Field()
     answer_date = scrapy.Field()
     answer_text = scrapy.Field()
